608cdec9d73b1988d0953954ac6c6098d70d06b9
NVai <reyes.s.ign@gmail.com>
2021-12-09 00:01:31 -0300

    up button

--- nav.js
+++ nav.js
@@ -1,3 +1,4 @@
+/* RESPONSIVE MENU */
 document.querySelector('.menu-btn').addEventListener('click', () => {
     document.querySelector('.nav-menu').classList.toggle('show');
 });
@@ -10,3 +11,22 @@ ScrollReveal().reveal('.cards', {delay: 500});
 ScrollReveal().reveal('.cards-banner', {delay: 500});
 ScrollReveal().reveal('.social', {delay: 500});
 ScrollReveal().reveal('.footer-links', {delay: 500});
+
+/* UP BUTTON */
+$(document).ready(function(){
+
+	$('.ir-arriba').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
+
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.ir-arriba').slideDown(300);
+		} else {
+			$('.ir-arriba').slideUp(300);
+		}
+	});
+
+});
