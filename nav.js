fe8fb916964975cbaf154194d28144df871fce68
NVai <reyes.s.ign@gmail.com>
2024-08-02 19:14:48 -0400

    add falling words to proyects

--- nav.js
+++ nav.js
@@ -1,32 +1,33 @@
 /* RESPONSIVE MENU */
-document.querySelector('.menu-btn').addEventListener('click', () => {
-    document.querySelector('.nav-menu').classList.toggle('show');
+document.querySelector(".menu-btn").addEventListener("click", () => {
+  document.querySelector(".nav-menu").classList.toggle("show");
 });
 
 // SCROLL REVEAL
 
-ScrollReveal().reveal('.showcase');
-ScrollReveal().reveal('.aboutme', {delay: 500});
-ScrollReveal().reveal('.cards', {delay: 500});
-ScrollReveal().reveal('.cards-banner', {delay: 500});
-ScrollReveal().reveal('.social', {delay: 500});
-ScrollReveal().reveal('.footer-links', {delay: 500});
+ScrollReveal().reveal(".showcase");
+ScrollReveal().reveal(".aboutme", { delay: 500 });
+ScrollReveal().reveal(".cards", { delay: 500 });
+ScrollReveal().reveal(".cards-banner", { delay: 500 });
+ScrollReveal().reveal(".social", { delay: 500 });
+ScrollReveal().reveal(".footer-links", { delay: 500 });
 
 /* UP BUTTON */
-$(document).ready(function(){
+$(document).ready(function () {
+  $(".ir-arriba").click(function () {
+    $("body, html").animate(
+      {
+        scrollTop: "0px",
+      },
+      300
+    );
+  });
 
-	$('.ir-arriba').click(function(){
-		$('body, html').animate({
-			scrollTop: '0px'
-		}, 300);
-	});
-
-	$(window).scroll(function(){
-		if( $(this).scrollTop() > 0 ){
-			$('.ir-arriba').slideDown(300);
-		} else {
-			$('.ir-arriba').slideUp(300);
-		}
-	});
-
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 0) {
+      $(".ir-arriba").slideDown(300);
+    } else {
+      $(".ir-arriba").slideUp(300);
+    }
+  });
 });
